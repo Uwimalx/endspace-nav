7a992ad8f9bb58e5b43f0af5bf6ceb9ccb1729ed
Uwimalx <akased@qq.com>
2026-07-19 17:34:49 +0800

    Implement favicon logic in App component
    Add useEffect to set favicon based on site config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GlobalProvider } from '@/lib/global'
 import { ThemeProvider } from '@/lib/theme.jsx'
+import { siteConfig } from '@/config'
 import HomePage from '@/pages/HomePage'
 import NotFoundPage from '@/pages/NotFoundPage'
 
 function App() {
+  useEffect(() => {
+    const avatar = siteConfig('AVATAR')
+    if (!avatar) return
+    let link = document.querySelector("link[rel='icon']")
+    if (!link) {
+      link = document.createElement('link')
+      link.rel = 'icon'
+      document.head.appendChild(link)
+    }
+    link.href = avatar
+  }, [])
+
   return (
     <GlobalProvider>
       <ThemeProvider>
